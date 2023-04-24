9c858dfad1cc08a9fd469e7f8effc56ef908c9fe
Bryan | JJ <with.beth@gmail.com>
2023-04-25 02:05:41 +0900

    refactor(DaoFactory): DaoFactory를 설정정보로 활용해 Spring IoC Container사용

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -3,14 +3,19 @@ package springbook.user.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        User newUser = new User("1", "withbeth", "qwer");
 
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = ac.getBean(UserDao.class);
+
+        User newUser = new User("1", "withbeth", "qwer");
 
         dao.add(newUser);
 
